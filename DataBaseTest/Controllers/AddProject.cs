46acdeee4c03c3bcd92e37ac73461a96930584a0
Serj <serjdesign.13@gmail.com>
2021-12-21 09:14:28 +0300

    Добавьте файлы проекта.

---
+++ DataBaseTest/Controllers/AddProject.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using DataBaseTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using DataBaseTest.Models;
+
+namespace DataBaseTest.Controllers
+{
+    public class AddProject : Controller
+    {
+        public ActionResult Index()
+        {
+            classAddProject[] m_info = {
+                new classAddProject { id = 1, name = "test project name", description = "test description", email = "test@email.com"},
+                new classAddProject { id = 2, name = "test project name", description = "test description", email = "test@email.com"},
+                new classAddProject { id = 3, name = "test project name", description = "test description", email = "test@email.com"}
+            };
+
+            ViewBag.Fun = m_info;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add(int id)
+        {
+
+            ViewBag.t = "test";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+
+            ViewBag.t = "test";
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ DataBaseTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataBaseTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
