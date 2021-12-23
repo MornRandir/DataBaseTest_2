9bb7f69ac3b54d05a06f0f5e04cb044c62cd73e5
Serj <serjdesign.13@gmail.com>
2021-12-23 04:28:07 +0300

    SomeTests nothig works again

--- DataBaseTest/Controllers/OrderController.cs
+++ DataBaseTest/Controllers/OrderController.cs
@@ -15,67 +15,15 @@ namespace DataBaseTest.Controllers
     public class OrderController : Controller
     {
 
-        static readonly Dictionary<Guid, classOrder> updates = new Dictionary<Guid, classOrder>();
+
+
 
         [HttpPost]
-        [ActionName("AddOrder")]
-        public HttpResponseMessage PostComplex(classOrder update)
+        public IActionResult Index(string name, string tasks, string email)
         {
-            if (ModelState.IsValid && update != null)
-            {
-                // Convert any HTML markup in the status text.
-                update.name = HttpUtility.HtmlEncode(update.name);
-
-                // Assign a new ID.
-                var id = Guid.NewGuid();
-                updates[id] = update;
-
-                // Create a 201 response.
-                var response = new HttpResponseMessage(HttpStatusCode.Created)
-                {
-                    Content = new StringContent(update.name)
-                };
-                response.Headers.Location =
-                    new Uri(Url.Link("DefaultApi", new { action = "name", id = id }));
-                return response;
-            }
-            else
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
-            }
+            string addProjectData = $"Name: {name}  Tasks: {tasks} Email: {email}";
+            return Content(addProjectData);
         }
-
-        [HttpGet]
-        public classOrder name(Guid id)
-        {
-            classOrder update;
-            if (updates.TryGetValue(id, out update))
-            {
-                return update;
-            }
-            else
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-        }
-
-
-
-        public IActionResult Index()
-        {
-            using (var context = new OrderContext())
-            {
-                var order = new classOrder { id = 1, name = "test project name", tasks = "test description", email = "test@email.com" };
-                context.Orders.Add(order);
-                context.SaveChanges();
-
-                return View();
-
-            }
-
-        }
-
-
 
 
 
